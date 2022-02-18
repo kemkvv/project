bf513ced48db04b4b6d936161f33cdcb0509ab4c
kemkvv <kem-cv@yandex.ru>
2022-02-18 14:23:45 +0300

    project update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,69 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 3,
-    centeredSlides: true,
-    loopedSlides: 6,
+//Бургер-меню
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+    navigation.classList.add('navigation_active');
 });
 
-sliderThumbs.on('click', (swiper) => {
-    swiper.slideTo(swiper.clickedIndex)
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active');
 });
 
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    spaceBetween: 10,
-    loopedSlides: 6,
+//Фоновая музыка
+try {
+const mute = document.querySelector('.mute__checkbox');
+const audio = new Audio('audio/waterTower.mp3');
+
+mute.addEventListener('change', () => {
+    if (mute.checked) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+})}
+catch {
+    console.log('На странице нет фоновой музыки')
+}
+
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 3,
+        centeredSlides: true,
+        loopedSlides: 4,
+    });
+
+    sliderThumbs.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex)
+    });
+
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        spaceBetween: 10,
+        loopedSlides: 4,
+    });
+
+    sliderThumbs.controller.control = sliderMain;
+    sliderMain.controller.control = sliderThumbs;
+
+const videos = document.querySelectorAll('video');
+
+sliderMain.on('slideChange', () => {
+    for (let i = 0; videos.length; i += 1) {
+        videos[i].pause();
+    }
 });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+const pagination = document.querySelector('.pagination');
+const paginationButton = document.querySelector('.pagination__arrow');
+
+paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination_active')
+});
+
+} catch {
+    console.log('На странице нет слайдера')
+}
